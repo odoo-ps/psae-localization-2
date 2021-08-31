7ecf8c52544f63b612e37bb25dc96940bd02ccb3
sal-odoo <sal@odoo.com>
2021-08-31 12:35:00 +0400

    [ADD] l10n_sa_account_reports

---
+++ l10n_sa_accout_reports/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Saudi Arabia - Account Reports',
+    'version': '14.0.1.0.0',
+    'author': 'Odoo PS',
+    'category': 'Accounting/Localizations',
+    'description': """
+    Financial Reports for Saudi Arabia
+""",
+    'depends': ['account_reports', 'l10n_sa'],
+    'data': [
+        'data/account_financial_report_data.xml',
+    ],
+}
