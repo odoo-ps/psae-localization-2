64a26a46f6553c30625a63d7c400e73dd632900f
sal-odoo <sal@odoo.com>
2021-09-02 14:57:15 +0400

    [ADD] all uae devs

---
+++ l10n_ae_hr_payroll/models/hr_employee.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+
+class HREmployee(models.Model):
+    _inherit = "hr.employee"
+
+    end_employment = fields.Boolean()
+    end_of_service = fields.Boolean()
+    resigned = fields.Boolean()
