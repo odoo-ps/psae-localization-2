83e47167a522f957c0bc03d38a19ff82cd8554f7
baa <baa@odoo.com>
2021-11-10 16:08:53 +0400

    [IMP]

--- pos_l10n_gcc/static/src/js/OrderReceipt.js
+++ pos_l10n_gcc/static/src/js/OrderReceipt.js
@@ -4,17 +4,17 @@ odoo.define('pos_l10n_gcc.OrderReceipt', function (require) {
     const OrderReceipt = require('point_of_sale.OrderReceipt')
     const Registries = require('point_of_sale.Registries');
 
-    const OrderReceiptGCCA = OrderReceipt =>
+    const OrderReceiptGCC = OrderReceipt =>
         class extends OrderReceipt {
 
             get receiptEnv() {
                 let receipt_render_env = super.receiptEnv;
                 let receipt = receipt_render_env.receipt;
-                receipt.is_gcc_country = ['SA', 'AE', 'BH', 'OM', 'QA', 'KW'].includes(this._receiptEnv.order.pos.company.country.code);
+                receipt.is_gcc_country = ['SA', 'AE', 'BH', 'OM', 'QA', 'KW'].includes(receipt_render_env.order.pos.company.country.code);
                 return receipt_render_env;
             }
 
         }
-    Registries.Component.extend(OrderReceipt, OrderReceiptGCCA)
-    return OrderReceiptGCCA
+    Registries.Component.extend(OrderReceipt, OrderReceiptGCC)
+    return OrderReceiptGCC
 });
--- pos_l10n_sa/static/src/js/OrderReceipt.js
+++ pos_l10n_sa/static/src/js/OrderReceipt.js
@@ -51,51 +51,11 @@ odoo.define('pos_l10n_sa.OrderReceipt', function (require) {
             }
 
             _compute_qr_code_field(tag, field) {
-                const company_name_byte_array = this._toUTF8Array(field);
-                const company_name_tag_encoding = this._longToByteArray(tag);
-                const company_name_length_encoding = this._longToByteArray(company_name_byte_array.length);
-                return company_name_tag_encoding.concat(company_name_length_encoding, company_name_byte_array);
-            }
-
-            _toUTF8Array(str) {
-                // https://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
-                var utf8 = [];
-                for (var i = 0; i < str.length; i++) {
-                    var charcode = str.charCodeAt(i);
-                    if (charcode < 0x80) utf8.push(charcode);
-                    else if (charcode < 0x800) {
-                        utf8.push(0xc0 | (charcode >> 6),
-                            0x80 | (charcode & 0x3f));
-                    } else if (charcode < 0xd800 || charcode >= 0xe000) {
-                        utf8.push(0xe0 | (charcode >> 12),
-                            0x80 | ((charcode >> 6) & 0x3f),
-                            0x80 | (charcode & 0x3f));
-                    }
-                    // surrogate pair
-                    else {
-                        i++;
-                        // UTF-16 encodes 0x10000-0x10FFFF by
-                        // subtracting 0x10000 and splitting the
-                        // 20 bits of 0x0-0xFFFFF into two halves
-                        charcode = 0x10000 + (((charcode & 0x3ff) << 10)
-                            | (str.charCodeAt(i) & 0x3ff));
-                        utf8.push(0xf0 | (charcode >> 18),
-                            0x80 | ((charcode >> 12) & 0x3f),
-                            0x80 | ((charcode >> 6) & 0x3f),
-                            0x80 | (charcode & 0x3f));
-                    }
-                }
-                return utf8;
-            }
-
-            _longToByteArray(long) {
-                var bytes = [];
-                var i = 8;
-                do {
-                    bytes[--i] = long & (255);
-                    long = long >> 8;
-                } while (i)
-                return bytes.reverse().slice(0, 1);
+                const textEncoder = new TextEncoder();
+                const name_byte_array = Array.from(textEncoder.encode(field));
+                const name_tag_encoding = [tag];
+                const name_length_encoding = [name_byte_array.length];
+                return name_tag_encoding.concat(name_length_encoding, name_byte_array);
             }
         }
     Registries.Component.extend(OrderReceipt, OrderReceiptQRCodeSA)
